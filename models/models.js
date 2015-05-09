8c77c839a88df154401ef67e87b422719514372e
sirasistant <sirasistant@gmail.com>
2015-05-09 17:07:41 +0200

    Added dependencies to BBDD libraries on package.json and added database js files on models folder

---
+++ models/models.js
@@ -0,0 +1,20 @@
+var path =require('path');
+
+var Sequelize=require('Sequelize');
+
+var sequelize=new Sequelize(null,null,null,{dialect:"sqlite",sqlite:"quiz.sqlite"});
+
+var Quiz = sequelize.import(path.join(__dirname,"quiz"));
+
+exports.Quiz=Quiz;
+
+sequelize.sync().success(function(){
+	Quiz.count().success(function(count){
+		if(count===0){
+			Quiz.create({
+				pregunta:"Capital de Italia",
+				respuesta:"Roma"
+			}).success(function(){console.log("Base de datos inicializada")});
+		}
+	});
+});
