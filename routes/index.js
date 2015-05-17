b2debe01d07cf9c340dbe1f94c7de1db7b0cd192
sirasistant <sirasistant@gmail.com>
2015-05-17 10:48:36 +0200

    now questions can be edited

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,11 @@ router.param('quizId', quizController.load);
 router.get('/quizes',quizController.index);
 router.get('/quizes/new',quizController.new);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/author', function (req, res, next) {
 	res.render('author', {authors: 'Creadores', author1: {name: 'Álvaro Rodríguez', photo: "images/alvaro.jpg"}, author2: { name: 'Gregorio Juliana', photo: "images/gregorio.jpg"},errors:[]});
 });
 router.post('/quizes/create',quizController.create);
-
+router.put('/quizes/:quizId(\\d+)',quizController.update);
 module.exports = router;
