5df24d6ea52cb33337b1926990fb9f5586a7044e
sirasistant <sirasistant@gmail.com>
2015-05-19 12:47:20 +0200

    Now requires auth for elevated actions

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,16 @@ router.post('/login',sessionController.create);
 router.get('/logout',sessionController.destroy);
 
 router.get('/quizes',quizController.index);
-router.get('/quizes/new',quizController.new);
+router.get('/quizes/new',sessionController.loginRequired,quizController.new);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
-router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
+router.get('/quizes/:quizId(\\d+)/edit',sessionController.loginRequired,quizController.edit);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/author', function (req, res, next) {
 	res.render('author', {authors: 'Creadores', author1: {name: 'Álvaro Rodríguez', photo: "images/alvaro.jpg"}, author2: { name: 'Gregorio Juliana', photo: "images/gregorio.jpg"},errors:[]});
 });
-router.post('/quizes/create',quizController.create);
-router.put('/quizes/:quizId(\\d+)',quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.post('/quizes/create',sessionController.loginRequired,quizController.create);
+router.put('/quizes/:quizId(\\d+)',sessionController.loginRequired,quizController.update);
+router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
