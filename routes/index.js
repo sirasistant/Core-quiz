153709d23edacb3d05723933a3b8fc17fb489247
sirasistant <sirasistant@gmail.com>
2015-05-19 12:22:17 +0200

    now supports users

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,21 @@ var router = express.Router();
 
 var quizController=require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController=require('../controllers/session_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' ,
       errors:[]});
 });
+
 router.param('quizId', quizController.load);
+
+router.get('/login',sessionController.new);
+router.post('/login',sessionController.create);
+router.get('/logout',sessionController.destroy);
+
 router.get('/quizes',quizController.index);
 router.get('/quizes/new',quizController.new);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
